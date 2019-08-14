e5612319f7653d30db7849c5fde10183b2cf00c7
soodung2 <37910781+soodung2@users.noreply.github.com>
2019-08-14 16:54:08 +0900

    Revert "Soojung"

--- dbtestweb_6/src/connectRoutes.js
+++
@@ -1,9 +0,0 @@
-var routes = require('../routes/index');
-var time5 = require('../routes/time5');
-var timeB2 = require('../routes/timeB2');
-
-module.exports = function connectRoutes(app) {
-  app.use('/', routes);
-  app.use('/', time5);
-  app.use('/', timeB2);
-};
