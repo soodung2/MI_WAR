5a01e6b0a42820ca5451e2601af309f6997c08ca
soodung2 <37910781+soodung2@users.noreply.github.com>
2019-08-14 18:06:17 +0900

    브랜치두개 합친거
    윤이씨 브랜치랑 수정 브랜치 합친 결과물 웹
    view 랑 route파일에서 앞에 _ 붙은게 기존의 상세 페이지 부분인데 윤이씨가 수정한 부분이랑 충돌나서 일단 이름만 바꿔 뒀습니다

--- dbtestweb_6/routes/time5.js
+++ dbtestweb_6/routes/time5.js
@@ -8,18 +8,41 @@ Client.connect('mongodb://ec2-13-125-244-112.ap-northeast-2.compute.amazonaws.co
     return;
     console.log(error);
   }//버튼 누를때 db접속
+  /*router.get('/',function(req,res,next){
+    db1.collection('out').find(function(err,docs){
+      res.render('time5',{nowdata:docss});
+    });
+  });*/
   router.get('/time5', function (req, res, next) {
     var db1 = client;//db1에 db받아온거 넣어줌
-    db1.collection('out').find({})//db 받아온거에서 out 컬렉션에있는거 find로 다 불러옴 (findOne 함수쓰면 골라서 가져올수있음)
+    var mysort = {NOW:-1};//시간을 역순으로 정렬하는 변수선언
+    var projection={_id: 0,NOW: 1,Temp: 1,PM10: 1,Humi: 1,PM25: 1};//id값을 생략해서 보여주는 변수
+    db1.collection('out').find({},projection).sort(mysort).limit(1)//db/out에서 <시간역순정렬(mysort)>  후   <값1개(limit(1))>  받아옴(가장 최근 값받아옵니다.)
     .toArray(function(err, docs) {
       if (err) {
         next(err);
         return;
-      }
-      res.render('time5', {list: docs});//받아온거 time5.ejs로 넘김
+    }
+      res.render('time5', {nowdata: docs[0].NOW,tempdata: docs[0].Temp,humidata: docs[0].Humi,pm25data: docs[0].PM25,pm10data: docs[0].PM10});//받아온거 time5.ejs로 넘김
+      //res.render('time5', {tempdata: docs[1].Temp});
+      //res.render('time5', {humidata: docs[2].Humi});
+      //res.render('time5', {pm25data: docs[3].PM25});
+      //res.render('time5', {pm10data: docs[4].PM10});
     });
-  });
+    /*db1.collection('out').findOne({},projection,function(err,docs){
+      if (err) {
+        next(err);
+        return;
+      }
+      res.render('time5', {nowdata: docs.NOW,tempdata: docs.Temp,humidata: docs.Humi,pm25data: docs.PM25,pm10data: docs.PM10});//받아온거 time5.ejs로 넘김
+      res.render('time5', {tempdata: docs.Temp});
+      res.render('time5', {humidata: docs.Humi});
+      res.render('time5', {pm25data: docs.PM25});
+      res.render('time5', {pm10data: docs.PM10});
+  });*/
 });
+});
+module.exports = router;
     /*router.get('/time5', function (req, res, next) {
       db1 = client.db;
       db1.collection('out').find({})
@@ -65,5 +88,3 @@ router.get('/time5', function (req, res, next) {
     res.render('time5', {list: docs});
   });
 });*/
-
-module.exports = router;
--- dbtestweb_6/routes/timeB2.js
+++ dbtestweb_6/routes/timeB2.js
@@ -9,14 +9,16 @@ Client.connect('mongodb://ec2-13-125-244-112.ap-northeast-2.compute.amazonaws.co
     console.log(error);
   }
   router.get('/timeB2', function (req, res, next) {
-    var db1 = client;
-    db1.collection('out').find({})
+    var db1 = client;//db1에 db받아온거 넣어줌
+    var mysort = {NOW:-1};//시간을 역순으로 정렬하는 변수선언
+    var projection={_id: 0,NOW: 1,Temp: 1,PM10: 1,Humi: 1,PM25: 1};//id값을 생략해서 보여주는 변수
+    db1.collection('out').find({},projection).sort(mysort).limit(1)//db/out에서 <시간역순정렬(mysort)>  후   <값1개(limit(1))>  받아옴(가장 최근 값받아옵니다.)
     .toArray(function(err, docs) {
       if (err) {
         next(err);
         return;
-      }
-      res.render('timeB2', {list: docs});
+    }
+      res.render('timeB2', {nowdata: docs[0].NOW,tempdata: docs[0].Temp,humidata: docs[0].Humi,pm25data: docs[0].PM25,pm10data: docs[0].PM10});
     });
   });
 });
