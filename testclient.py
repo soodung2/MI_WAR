a25a1a8edaf39c3eba309a9a5817b1f1192863c7
hjs1212 <gjwlsthf1020@gmail.com>
2019-10-28 01:10:51 +0900

    Regresssion testfile

---
+++ testclient.py
@@ -0,0 +1,16 @@
+import json
+from socket import *
+
+HOST = 'ec2-54-180-155-65.ap-northeast-2.compute.amazonaws.com'
+PORT = 8080
+ADDR = (HOST,PORT)
+client = socket(AF_INET, SOCK_STREAM)
+
+if __name__ == '__main__' :
+    client.connect(ADDR)
+    print(1)
+    client.sendall(json.dumps(['4040','s1','n1']).encode('utf-8'))
+    print(2)
+    msg = json.loads(client.recv(1024).decode('utf-8'))
+    print(3)
+    print(msg)
